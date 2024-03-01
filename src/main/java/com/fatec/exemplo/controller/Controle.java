97442a467f2216e2d30bd8f1b87f8cba90e4b998
Pablo Santos <opablosantos@gmail.com>
2024-02-29 22:23:29 -0300

    Realização de testes de endpoints CRUD

--- src/main/java/com/fatec/exemplo/controller/Controle.java
+++ src/main/java/com/fatec/exemplo/controller/Controle.java
@@ -7,9 +7,17 @@ import com.fatec.exemplo.model.Cliente;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.PutMapping;
+
+
+
 
 
 
@@ -20,8 +28,8 @@ public class Controle {
         return "Alô, Mundo!";
     }
     
-    @GetMapping("/cliente")
-    public Cliente getCliente() {
+    @GetMapping("/cliente-teste")
+    public Cliente getClienteTeste() {
         return new Cliente(1, "José", "Rua Frei João");
     }
 
@@ -39,6 +47,29 @@ public class Controle {
         return new Cliente(clienteID, "Souza", "Avenida. Aguia de Haia");
     }
     
+    @PostMapping("/clientes")
+    public Cliente cadastrarCliente(@RequestBody Cliente cliente) {
+        List<Cliente> lista = new ArrayList<>();
+        lista.add(new Cliente (1, cliente.getNome(), cliente.getEndereco()));
+        return lista.get(0);
+    }
+
+    @PutMapping("/clientes/{id}/update")
+    public Cliente atualizaCliente(@RequestBody Cliente cliente, @PathVariable("id") int id) {
+        Cliente clienteAtualizado = new Cliente(id, cliente.getNome(), cliente.getEndereco());
+        return clienteAtualizado;
+    }
     
+    @DeleteMapping("/cliente/{id}/delete")
+    public String deleteCliente(@PathVariable ("id") int id) {
+        return "Cliente id: " + id + " excluído!";
+    }
+
+    @GetMapping("/cliente")
+    public ResponseEntity<Cliente> getCliente() {
+        Cliente cliente = new Cliente (1, "José", "Rua Bresser");
+        // return ResponseEntity.ok(cliente);
+        return new ResponseEntity<>(cliente, HttpStatus.OK);
+    }
     
 }
